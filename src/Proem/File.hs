74eb50c125111789668c6268363d494bf24caccb
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-11-26 12:05:04 -0500

    generalize File.read / File.write to MonadIO

--- src/Proem/File.hs
+++ src/Proem/File.hs
@@ -15,6 +15,7 @@ where
 import Control.Exception (IOException)
 import qualified Control.Exception as Exception
 import Control.Monad (when)
+import Control.Monad.IO.Class (MonadIO (..))
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Internal as ByteString.Internal
 import qualified Data.ByteString.Unsafe as ByteString.Unsafe
@@ -52,49 +53,51 @@ path =
 -- | Read a file.
 -- FIXME make this non-blocking
 -- FIXME lock file? (or otherwise handle race conditions)
-read :: Path -> IO (Either IOException ByteString)
+read :: MonadIO m => Path -> m (Either IOException ByteString)
 read p =
-  Exception.try do
-    Exception.bracket
-      ( Posix.openFd
-          (coerce @OsString.Internal.OsString @OsString.Internal.PosixString p)
-          Posix.ReadOnly
-          Posix.defaultFileFlags
-      )
-      Posix.closeFd
-      \fd -> do
-        let loop :: C.CSize -> Ptr.Ptr Word8 -> IO ()
-            loop n ptr = do
-              m <- Posix.fdReadBuf fd ptr n
-              when (m < n) (loop (n - m) (Ptr.plusPtr ptr (unsafeCSizeToInt m)))
-        status <- Posix.getFdStatus fd
-        let Posix.COff size64 = Posix.fileSize status
-        ByteString.Internal.create
-          (unsafeInt64ToInt size64)
-          (loop (int64ToCSize size64))
+  liftIO do
+    Exception.try do
+      Exception.bracket
+        ( Posix.openFd
+            (coerce @OsString.Internal.OsString @OsString.Internal.PosixString p)
+            Posix.ReadOnly
+            Posix.defaultFileFlags
+        )
+        Posix.closeFd
+        \fd -> do
+          let loop :: C.CSize -> Ptr.Ptr Word8 -> IO ()
+              loop n ptr = do
+                m <- Posix.fdReadBuf fd ptr n
+                when (m < n) (loop (n - m) (Ptr.plusPtr ptr (unsafeCSizeToInt m)))
+          status <- Posix.getFdStatus fd
+          let Posix.COff size64 = Posix.fileSize status
+          ByteString.Internal.create
+            (unsafeInt64ToInt size64)
+            (loop (int64ToCSize size64))
 
 -- | Write a file.
 -- FIXME make this non-blocking
-write :: Path -> ByteString -> IO (Either IOException ())
+write :: MonadIO m => Path -> ByteString -> m (Either IOException ())
 write p contents =
-  Exception.try do
-    Exception.bracket
-      ( Posix.openFd
-          (coerce @OsString.Internal.OsString @OsString.Internal.PosixString p)
-          Posix.WriteOnly
-          Posix.defaultFileFlags
-            { Posix.creat = Just 0o666,
-              Posix.trunc = True
-            }
-      )
-      Posix.closeFd
-      \fd -> do
-        let loop :: C.CSize -> Ptr Word8 -> IO ()
-            loop n ptr = do
-              m <- Posix.fdWriteBuf fd ptr n
-              when (m < n) (loop (n - m) (Ptr.plusPtr ptr (unsafeCSizeToInt m)))
-        ByteString.Unsafe.unsafeUseAsCStringLen contents \(ptr, len) ->
-          loop (intToCSize len) (Ptr.castPtr @C.CChar @Word8 ptr)
+  liftIO do
+    Exception.try do
+      Exception.bracket
+        ( Posix.openFd
+            (coerce @OsString.Internal.OsString @OsString.Internal.PosixString p)
+            Posix.WriteOnly
+            Posix.defaultFileFlags
+              { Posix.creat = Just 0o666,
+                Posix.trunc = True
+              }
+        )
+        Posix.closeFd
+        \fd -> do
+          let loop :: C.CSize -> Ptr Word8 -> IO ()
+              loop n ptr = do
+                m <- Posix.fdWriteBuf fd ptr n
+                when (m < n) (loop (n - m) (Ptr.plusPtr ptr (unsafeCSizeToInt m)))
+          ByteString.Unsafe.unsafeUseAsCStringLen contents \(ptr, len) ->
+            loop (intToCSize len) (Ptr.castPtr @C.CChar @Word8 ptr)
 
 --
 
